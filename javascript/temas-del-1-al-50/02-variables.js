89b5328c27faa42a882064d5264cd81983bc85eb
PalominoDev28 <danielpalomino2806@gmail.com>
2025-07-04 23:55:51 -0500

    Día 2 - Tipos de variables en js

---
+++ javascript/temas-del-1-al-50/02-variables.js
@@ -0,0 +1,32 @@
+/* 
+Tipos de datos:
+- String: se utiliza para representar texto.
+- Number: se utiliza para representar números.
+- Boolean: se utiliza para representar valores lógicos (true o false).
+- Array: se utiliza para representar listas de datos.
+- Object: se utiliza para representar objetos.
+- Function: se utiliza para representar funciones.
+- undefined: se utiliza para representar valores indefinidos.
+- null: se utiliza para representar valores nulos.
+
+Notas:
+- Las variables se declaran con var, let o const.
+- Las variable se declaran con un nombre que sea significativo.
+- Las variables se declaran en Js con un nombre que sea camelCase.
+
+*/
+var miVariable = "PalominoDev"; // String
+var miNumero = 10; // Number
+var miBooleano = true; // Boolean
+var miArray = [1, 2, 3, 4, 5]; // Array
+var miObjeto = { nombre: "Palomino", edad: 20 }; // Object
+var miFuncion = function () {}; // Function
+
+console.log(miVariable);
+console.log(miNumero);
+console.log(miBooleano);
+console.log(miArray);
+console.log(miObjeto);
+console.log(miFuncion);
+
+
